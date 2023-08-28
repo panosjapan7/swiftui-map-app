fa3c96f2e8f8c87ad761f6d188d838e4d0bb080d
panosjapan7 <panosjapan@gmail.com>
2023-08-28 09:06:06 +0200

    Initial Commit

---
+++ SwiftfulMapApp/SwiftfulMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulMapAppApp.swift
+//  SwiftfulMapApp
+//
+//  Created by Panagiotis Tsapanidis on 2023-08-28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
